139e9c6e338fda208c0205fbad485c406b705b33
Cindy Nguyen <Cin-Ng@users.noreply.github.com>
2023-08-18 03:22:59 +0000

    leap year function changed

--- test_leap.c
+++ test_leap.c
@@ -8,13 +8,17 @@
  */
 int is_leap(long year) {
 
-  if (year % 4 != 0) {
-    return 0;
-  }
-
-  if (year % 100 == 0) {
+  if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
     return 1;
   }
+
+  /*if (year % 100 == 0) {
+    return 0;
+  }
+  if (year % 400 == 0) {
+    return 1;
+  }*/
+
 
   return 0;
 }
